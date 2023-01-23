91a773b5e37d1ed133bf44e309a40d90de739a8e
ogwaanto <ogwannto@gmail.com>
2023-01-23 22:36:21 +0300

    Fix 3

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
-
-
 def safe_print_division(a, b):
     try:
         div = a / b
@@ -9,4 +7,3 @@ def safe_print_division(a, b):
     finally:
         print("Inside result: {}".format(div))
     return (div)
-    
